33adb66e790b898a037444a1ed4252368c4b20fd
jrsorensen <jack.r.sorensen@gmail.com>
2023-12-13 21:16:58 -0700

    login page looks right. login and register buttons do not do anything rn

--- src/app.jsx
+++ src/app.jsx
@@ -9,32 +9,23 @@ import './app.css';
 export default function App() {
   return (
     <BrowserRouter>
-        <div className='body bg-dark text-light'>
+       
+        <div className='body'>
+        <header>
+            <span className='day-buttons'> 
+                <NavLink className='submit' to='workout'>Chest</NavLink>
+                <NavLink className='submit' to='workout'>Hamstring</NavLink>
+                <NavLink className='submit' to='workout'>Back</NavLink>
+                <NavLink className='submit' to='workout'>Shouders</NavLink>
+                <NavLink className='submit' to='workout'>Quads</NavLink>
+                <NavLink className='submit' to='workout'>Arms</NavLink>
+            </span>
+            <span className="page-buttons"> 
+              <NavLink className='submit' to=''>Log out</NavLink>
+              <NavLink className='submit' to='progress'>Progress</NavLink>
+          </span>
+     </header>
 
-        <header className='container-fluid'>
-        <nav className='navbar fixed-top navbar-dark'>
-          <div className='navbar-brand'>
-            Simon<sup>&reg;</sup>
-          </div>
-          <menu className='navbar-nav'>
-            <li className='nav-item'>
-              <NavLink className='nav-link' to=''>
-                Login
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink className='nav-link' to='workout'>
-                Workout
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink className='nav-link' to='progress'>
-                Progress
-              </NavLink>
-            </li>
-          </menu>
-        </nav>
-      </header>
 
       <Routes>
         <Route path='/' element={<Login />} exact />
@@ -42,39 +33,6 @@ export default function App() {
         <Route path='/progress' element={<Progress />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
-
-        {/* <header>
-           <div className="dayButtons"> 
-                 <button className="dayButton" onclick="location.href='/workout.html';">Chest</button>
-                 <button className="dayButton" onclick="location.href='/workout.html';">Hamstring</button>
-                 <button className="dayButton" onclick="location.href='/workout.html';">Back</button>
-                 <button className="dayButton" onclick="location.href='/workout.html';">Shoulders</button>
-                 <button className="dayButton" onclick="location.href='/workout.html';">Quads</button>
-                 <button className="dayButton" onclick="location.href='/workout.html';">Arms</button>
-             </div>
-             
-             <div className="navButtons"> 
-              <button type="submit" className="navigation" onclick="location.href='/index.html';">Log out</button>
-              <button type="sumbit" className="navigation" onclick="location.href='/progress.html';">Progress</button>
-          </div>
-      </header> */}
-
-          {/* <header>
-           <nav>
-             <div>Startup</div>
-             <menu>
-               <li>
-                 <NavLink to=''>Login</NavLink>
-               </li>
-               <li>
-                 <NavLink to='progress'>Progress</NavLink>
-               </li>
-               <li>
-                 <NavLink to='workout'>Workout</NavLink>
-               </li>
-             </menu>
-           </nav>
-         </header> */}
 
       <footer>
          <h1>Jack's page @CS 260</h1>
@@ -84,89 +42,6 @@ export default function App() {
     </BrowserRouter>
   );
 }
-
-// export default function App(){
-//   const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
-//   const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
-//   const [authState, setAuthState] = React.useState(currentAuthState);
-
-//   return (
-//     <BrowserRouter>
-//       <div>
-//         <header>
-//           <nav>
-//             <div>Startup</div>
-//             <menu>
-//               <li>
-//                 <NavLink to=''>Login</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to='progress'>Progress</NavLink>
-//               </li>
-//               <li>
-//                 <NavLink to='workout'>Workout</NavLink>
-//               </li>
-//             </menu>
-//           </nav>
-//         </header>
-
-//         <Routes>
-//           <Route path='/' element={<Login/>}/>
-//           <Route path='/progress' element={<Progress/>}/>
-//           <Route path='/workout' element={<Workout/>}/>
-//         </Routes>
-
-//         <footer>
-//           <div>
-//             <span>Jack Sorensen</span>
-//             <div>Link to github</div>
-//           </div>
-//         </footer>
-//       </div>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default function App() {
-//   const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
-//   const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
-//   const [authState, setAuthState] = React.useState(currentAuthState);
-
-//   return ( 
-//     <BrowserRouter>
-//         <div>
-//         <header>
-//           <div class="dayButtons"> 
-//                 <button class="dayButton" onclick="location.href='/workout.html';">Chest</button>
-//                 <button class="dayButton" onclick="location.href='/workout.html';">Hamstring</button>
-//                 <button class="dayButton" onclick="location.href='/workout.html';">Back</button>
-//                 <button class="dayButton" onclick="location.href='/workout.html';">Shoulders</button>
-//                 <button class="dayButton" onclick="location.href='/workout.html';">Quads</button>
-//                 <button class="dayButton" onclick="location.href='/workout.html';">Arms</button>
-//             </div>
-             
-//             <div class="navButtons"> 
-//              <button type="submit" class="navigation" onclick="location.href='/index.html';">Log out</button>
-//              <button type="sumbit" class="navigation" onclick="location.href='/progress.html';">Progress</button>
-//          </div>
-//      </header>
-
-//      <Routes>
-//       <Route path='/' element={<Login />} exact />
-//       <Route path='/workout' element={<Workout />} />
-//       <Route path='/progress' element={<Progress />} />
-//       <Route path='*' element={<NotFound />} />
-//      </Routes>
-
-//      <footer>
-//         <h1>Progress page @CS 260</h1>
-//         <a href="https://github.com/jrsorensen/cs-260-Fall-2023">My Github Repo</a>
-
-//     </footer>
-//   </div>
-//   </BrowserRouter>
-//   );
-// }
 
 function NotFound() {
   return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,143 @@
 import React from 'react';
+//import './login.css'
 
 export function Login() {
+
+  const proceed = false;
+const newLoginEvent = 'login';
+const newRegisterEvent = 'register';
+let socket;
+
+async function login() {
+    const name = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    try{
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify([name, password])
+      });
+      const resp = await response;
+      console.log(resp);
+      if (resp.status == 200){ 
+        localStorage.setItem("userName", name);
+        localStorage.setItem("password", password);
+        welcomeMessage();
+        
+        window.location.href = "/workout.html"
+        broadcastEvent(name, newLoginEvent);
+      }else{
+        alert("Username or password wrong");
+      }
+    }catch{
+      alert("error with login");
+    }
+  }
+
+function welcomeMessage() {
+        const message = "Welcome " + localStorage.getItem("userName") +
+         "! \n Are you ready to begin today's workout?"
+         alert(message)
+}
+
+function displayQuote(data) {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#quote');
+
+      const quoteEl = document.createElement('p');
+      const authorEl = document.createElement('p');
+ 
+      quoteEl.textContent = data.content;
+      authorEl.textContent = data.author;
+
+      containerEl.appendChild(quoteEl);
+      containerEl.appendChild(authorEl);
+    });
+}
+
+async function registerNew() {
+  const name = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  bod = [name, password];
+  try{
+    const response = await fetch('/api/create', {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify(bod)
+    });
+    const confirmation = await response;
+    if (confirmation.status == 200){ 
+      localStorage.setItem("userName", name);
+      localStorage.setItem("password", password);
+      welcomeMessage();
+      
+      window.location.href = "/workout.html"
+      broadcastEvent(name, newRegisterEvent);
+    }else{
+      alert('Username is already in use');
+    }
+  }catch{
+    alert("error with registration");
+  }
+}
+
+displayQuote();
+configureWebSocket();
+
+
+function configureWebSocket() {
+  const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+  socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+  socket.onopen = (event) => {
+     displayMsg('Welcome!', 'See who else is logging in.');
+  };
+  socket.onclose = () => {
+      displayMsg( 'workout', 'disconnected');
+  };
+  socket.onmessage = async (event) => {
+    const msg = JSON.parse(await event.data.text());
+    if (msg.type === newLoginEvent) {
+       displayMsg(msg.from, `has started their workout!`);
+    } else if (msg.type === newRegisterEvent) {
+       displayMsg(msg.from, `is starting their fitness journey!`);
+    }
+  };
+}
+
+function displayMsg(from, msg) {
+  const notificationText = document.getElementById("loginNotifications");
+  notificationText.innerHTML =
+    `<div class="event">${from} ${msg}</div>`;
+}
+
+function broadcastEvent(from, type) {
+  const event = {
+    from: from,
+    type: type
+  };
+  socket.send(JSON.stringify(event));
+}
+
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+    <main>
+      <div>
+          <form>
+              <label for="username">username:</label>
+              <input type="text" id="username"/><br/>
+              <label for="password">password:</label>
+              <input type="password" id="password"/><br/><br/>
+              <input type="button" class="submit" id="submit" value="Log in" onclick="login()"/>
+              <input type="button" class="submit" id="register" value="Register" onclick="registerNew()"/>
+          </form>
+          <p id="loginNotifications">
+            This is a placeholder for where my websocket will notify the user of when other users are logging in
+          </p>
+      </div>
+    
+      <div id="quote"></div>
     </main>
   );
 }
